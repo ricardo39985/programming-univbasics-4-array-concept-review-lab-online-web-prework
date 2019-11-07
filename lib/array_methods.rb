5f0c0759716d35f78d6bf295306cb5612d3629a6
Ricardo Persaud <ricardo39985@gmail.com>
2019-11-07 06:08:07 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,18 @@
 def find_element_index(array, value_to_find)
   # Add your solution here
+  array.lenght.times do |index|
+    if  array.include? (value_to_find)
+      return array.index(value_to_find)
+    else
+      return nil
 end
 
 def find_max_value(array)
   # Add your solution here
+  return array.max
 end
 
 def find_min_value(array)
   # Add your solution here
+  array.min
 end
